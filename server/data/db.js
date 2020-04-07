9e24b758e46761c6755fdb459cd34724324a622e
chinobv <morenoresendez03>
2020-04-07 06:39:39 -0500

    intento subir a Heroku

---
+++ server/data/db.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+mongoose.Promise = global.Promise;
+const model = mongoose.model;
+const Schema = mongoose.Schema;
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect("mongodb+srv://Server1:LaPass0102xD@chat-appdb-ws0bf.mongodb.net/test?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useCreateIndex: true
+});
+
+const usersSchema = new Schema({
+	name: String,
+	username: String,
+	password: String,
+	rol: String
+});
+
+usersSchema.pre( 'save', function (next) {
+//Si tiene hash no se vuelva a hashear
+	if(!this.isModified('password')) {
+		return next();
+	}
+	bcrypt.genSalt( 10, ( error, salt ) => {
+		if(error) return next(error);
+		bcrypt.hash( this.password, salt, (error, hash) => {
+			if(error) return next(error);
+			this.password = hash;
+			next();
+		});
+	});
+});
+
+
+const Users = model( "users", usersSchema );
+
+const messagesSchema = new Schema({
+	user: mongoose.Types.ObjectId,
+	text: String,
+	date: Date
+});
+
+const Messages = model( "messages", messagesSchema );
+
+export { Users, Messages }
---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+import { resolvers } from './data/resolvers.js'
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+
+const app = express();
+
+const typeDefs = fs.readFileSync("./data/schema.gql", "utf8");
+
+const port = process.env.PORT || 4000;
+
+app.use('/', express.static(`${__dirname}/build`))
+
+const server = new ApolloServer({ typeDefs, resolvers, context: async({req}) => {
+	
+	const token = req.headers['authorization'];
+	
+	if(token !== "null"){
+		
+		try{
+	
+			const userAct = await jwt.verify( token ,process.env.SECRET );
+			
+			req.userAct = userAct;
+			//console.log(userAct)
+
+			return {
+				userAct
+			};
+
+
+		}catch (error){
+			console.error(error);
+		}
+
+	}
+
+	//console.log(userAct)
+} });
+
+server.applyMiddleware({ app });
+
+app.listen({ port }, () => console.log(`Server Runing ${server.graphqlPath}`));
