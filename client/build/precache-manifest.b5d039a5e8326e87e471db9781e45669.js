6a042137698ffa57d40631cbcf0aee6222fe69ad
chinobv <morenoresendez03>
2020-04-07 22:45:22 -0500

    preparando para Zeit now

---
+++ client/build/precache-manifest.b5d039a5e8326e87e471db9781e45669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176686b1740a8afea4b0709fc80f8fe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d290d177d301f1a1c652",
+    "url": "/static/css/2.3e39b1ad.chunk.css"
+  },
+  {
+    "revision": "09106f212af8bc865096",
+    "url": "/static/css/main.778f08b4.chunk.css"
+  },
+  {
+    "revision": "d290d177d301f1a1c652",
+    "url": "/static/js/2.3babe28e.chunk.js"
+  },
+  {
+    "revision": "f789cbcc7c4fddde194bb68ab1f02beb",
+    "url": "/static/js/2.3babe28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09106f212af8bc865096",
+    "url": "/static/js/main.fbcba6c9.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
